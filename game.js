a2c3cdab5739a8cf8295ea9b6dfe96caf9837e82
DAVID <davidopara2019@gmail.com>
2023-10-14 14:28:06 +0100

    Updated

---
+++ game.js
@@ -0,0 +1,51 @@
+const question = document.querySelector('#question');
+const choices = Array.from(document.querySelectorAll('.choice-text'));
+const progressText = document.querySelector('#progressText');
+const scoreText = document.querySelector('#score');
+const progressBarFull = document.querySelector('#progressBarFull');
+
+
+
+
+let questions =  [
+    {
+        question: 'What is 2 + 2 ',
+        choice1 : '2',
+        choice2 : '4',
+        choice3 : '5',
+        choice4 : '7',
+        answer : 2,
+    },
+    {
+        question: 'What is 2 + 2 ',
+        choice1 : '2',
+        choice2 : '4',
+        choice3 : '5',
+        choice4 : '7',
+        answer : 2,
+    },
+    {
+        question: 'What is 2 + 2 ',
+        choice1 : '2',
+        choice2 : '4',
+        choice3 : '5',
+        choice4 : '7',
+        answer : 2,
+    },
+    {
+        question: 'What is 2 + 2 ',
+        choice1 : '2',
+        choice2 : '4',
+        choice3 : '5',
+        choice4 : '7',
+        answer : 2,
+    },
+    {
+        question: 'What is 2 + 2 ',
+        choice1 : '2',
+        choice2 : '4',
+        choice3 : '5',
+        choice4 : '7',
+        answer : 2,
+    },
+]
