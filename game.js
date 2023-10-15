1ed3396e9ae0f6b694fdeefef1663add144b1064
DAVID <davidopara2019@gmail.com>
2023-10-15 17:03:05 +0100

    Updated game.js

--- game.js
+++ game.js
@@ -83,7 +83,34 @@ getNewQuestion =  () => {
 
         const questionsindex = Math.floor(Math.random() * availableQuestions.length)
         currentQuestion = availableQuestions[questionsindex]
-        question.innerText = currentQuestion
+        question.innerText = currentQuestion.question
+
+        choices.forEach(choice => {
+            const number = choice.dataset['number']
+            choice.innerText = currentQuestion['choice'+number]
+        })
+
+        availableQuestions.slice(questionsindex, 1)
+
+        acceptingAnswers = true
 }
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if(!acceptingAnswers) return
+
+        acceptingAnswers = false
+        const selectedChoice = e.target
+        const selectedAnswer = selectedChoice.dataset['number']
+
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
+
+        if(classToApply === 'correct'){
+            incrementScore(SCORE_POINTS)
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply)
+    })
+})
 
 
