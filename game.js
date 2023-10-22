a3a8ed34b8985244e23b8c4ddc160b41726d90e9
DAVID <davidopara2019@gmail.com>
2023-10-22 11:56:08 +0100

    Updated game.js

--- game.js
+++ game.js
@@ -110,7 +110,20 @@ choices.forEach(choice => {
         }
 
         selectedChoice.parentElement.classList.add(classToApply)
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply)
+            getNewQuestion()
+        }, 1000)
     })
 })
+
+
+incrementScore = num => {
+    score+= num
+    scoreText.innerText = score
+}
+
+startgame()
 
 
